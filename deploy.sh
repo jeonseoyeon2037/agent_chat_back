1e9a06780638471ca799762e23959edd50107471
jeonseoyeon2037 <jseoyeon2037@gmail.com>
2025-06-16 11:11:59 +0900

    deploy.sh 수정2

--- deploy.sh
+++ deploy.sh
@@ -52,6 +52,13 @@ fi
 export PATH="/home/ubuntu/miniconda/bin:$PATH"
 source /home/ubuntu/miniconda/bin/activate
 
+# Update and install Nginx if not already installed
+if ! command -v nginx > /dev/null; then
+    echo "Installing Nginx"
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+
 # Nginx 설정
 echo "Configuring Nginx..."
 sudo bash -c 'cat > /etc/nginx/sites-available/myapp <<EOF
@@ -96,12 +103,6 @@ source /home/ubuntu/miniconda/bin/activate fastapi-env
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-# Update and install Nginx if not already installed
-if ! command -v nginx > /dev/null; then
-    echo "Installing Nginx"
-    sudo apt-get update
-    sudo apt-get install -y nginx
-fi
 
 # Nginx 설정 테스트 및 재시작
 echo "Testing and restarting Nginx..."
